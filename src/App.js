a506e9ced7b04777b4aa10ae0030a6eda54f6e79
iamarif1 <iamarif121@gmail.com>
2021-06-14 22:35:13 +0600

    Update many more things

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ImageCard from "./components/ImageCard/ImageCard";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=12940047-1ea793bd8bbe28b6f25f581d5&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>
-      <Navbar/>
-      <Home/>
-    </div>
+      <div className="container mx-auto">
+        { isLoading ? <h1 className="text-6xl text-center text-yellow-200 mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.key} image={image} />
+          ))}
+        </div>}
+      </div>
   );
 }
 

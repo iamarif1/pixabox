461921db17aeda3f3b7d9c52d6298c209860e4db
iamarif1 <iamarif121@gmail.com>
2021-06-15 06:34:35 +0600

    Use dotenv file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(() => {
     fetch(
-      `https://pixabay.com/api/?key=12940047-1ea793bd8bbe28b6f25f581d5&q=${term}&image_type=photo&pretty=true`
+      `${process.env.REACT_APP_PIXABAY_API_URL}?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -18,7 +18,6 @@ function App() {
       })
       .catch((err) => console.log(err));
   }, []);
-
   return (
       <div className="container mx-auto">
         { isLoading ? <h1 className="text-6xl text-center text-yellow-200 mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
